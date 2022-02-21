d5f51165fce495893ac59d205b5b483c6b82ede8
wanderson santos <wandersondasilvasantos18@hotmail.com>
2022-02-21 18:28:02 -0300

    Classe Pessoa

---
+++ src/Entities/People.java
@@ -0,0 +1,35 @@
+package Entities;
+
+
+public class People {
+
+	private static int counter = 1;
+
+	private String name;
+	private String cpf;
+
+	public People(String name, String cpf) {
+		
+		this.name = name;
+		this.cpf = cpf;
+		counter += 1;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
